297369de04d3e0d2e34b153f74bdf20db8271a65
Ruth Devlaeminck <ruth.devlaeminck@verizonwireless.com>
2017-12-05 12:01:31 -0600

    Change spgw to be kvm image instead of lxc image and fix interface issue

--- nfv/oaispgw/scripts/oaispgw_install.sh
+++ nfv/oaispgw/scripts/oaispgw_install.sh
@@ -12,13 +12,13 @@ source_defaults_file
 echo "127.0.1.1       $hostname.3gppnetwork.org $hostname" >> /etc/hosts
 
 MGMT_INTERFACE=$(getInterfaceName 1)
-SIGNAL_INTERFACE=$(getInterfaceName 2)
+SIGNAL_INTERFACE=$(getInterfaceName 1)
 
 create_interface_config_file "$SIGNAL_INTERFACE" >> $LOGFILE 2>&1
 
 mkdir $LOG_DIR
 source_generic_service_file "oaispgw" "install" "$SIGNAL_INTERFACE" "$MGMT_INTERFACE" "$mgmt_oa" >> $LOGFILE 2>&1
-update_config_file "SGW_INTERFACE_NAME_FOR_S11              = \"lo\"; " "SGW_INTERFACE_NAME_FOR_S11              = \"ens4\"; " $ETC_TARGET/spgw.conf   >> $LOGFILE 2>&1
+update_config_file "SGW_INTERFACE_NAME_FOR_S11              = \"lo\"; " "SGW_INTERFACE_NAME_FOR_S11              = \"$SIGNAL_INTERFACE\"; " $ETC_TARGET/spgw.conf   >> $LOGFILE 2>&1
 update_config_file "SGW_IPV4_ADDRESS_FOR_S11                = \"127.0.11.2\/8\";" "SGW_IPV4_ADDRESS_FOR_S11                = \"$mgmt_oa\/8\";" $ETC_TARGET/spgw.conf   >> $LOGFILE 2>&1
 
 

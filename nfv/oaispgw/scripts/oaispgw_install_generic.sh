0dd905756a730d5b8394ad8e8e82c02e131e44aa
Ruth Devlaeminck <ruth.devlaeminck@verizonwireless.com>
2017-12-05 12:00:55 -0600

    Change to read DNS from config file

--- nfv/oaispgw/scripts/oaispgw_install_generic.sh
+++ nfv/oaispgw/scripts/oaispgw_install_generic.sh
@@ -18,5 +18,5 @@ cp $OPENAIRCN_DIR/ETC/spgw.conf $ETC_TARGET
 update_config_file "SGW_INTERFACE_NAME_FOR_S1U_S12_S4_UP    = \"enp0s8\";" "SGW_INTERFACE_NAME_FOR_S1U_S12_S4_UP    = \"$SIGNAL_INTERFACE\";" $ETC_TARGET/spgw.conf 
 update_config_file "SGW_IPV4_ADDRESS_FOR_S1U_S12_S4_UP      = \"192.168.4.80\/24\";" "SGW_IPV4_ADDRESS_FOR_S1U_S12_S4_UP      = \"$SIGNAL_IP\/24\";" $ETC_TARGET/spgw.conf 
 update_config_file "PGW_INTERFACE_NAME_FOR_SGI            = \"enp0s3\";" "PGW_INTERFACE_NAME_FOR_SGI            = \"$WEB_INTERFACE\";" $ETC_TARGET/spgw.conf
-update_config_file "DEFAULT_DNS_IPV4_ADDRESS     = \"8.8.8.8\"" "DEFAULT_DNS_IPV4_ADDRESS     = \"10.143.96.62\"" $ETC_TARGET/spgw.conf
+update_config_file "DEFAULT_DNS_IPV4_ADDRESS     = \"8.8.8.8\"" "DEFAULT_DNS_IPV4_ADDRESS     = \"$DNS\"" $ETC_TARGET/spgw.conf
 

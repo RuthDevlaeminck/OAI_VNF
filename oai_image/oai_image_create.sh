8937c8c1f9629c0962030d99fe506e0849c2679b
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-12-08 16:31:07 -0600

    update to last version image create and prereq scripts

--- oai_image/oai_image_create.sh
+++ oai_image/oai_image_create.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+preq_script="prereq.sh"
+helper_script="build_helper2"
 
+THIS_SCRIPT_PATH=$(dirname $(readlink -f $0))
+
+if [ ! -f "$preq_script" ];then
+        echo "Missing $preq_script in this folder"
+fi
+
+if [ ! -f "$helper_script" ];then
+        echo "Missing $helper_script in this folder"
+fi
+
+echo "Installing specific linux kernels (v4.8/linux-headers-4.8.0-040800)"
 cd /tmp
 mkdir kernel
 cd kernel
@@ -10,14 +23,26 @@ wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.8/linux-image-4.8.0-040800
 sudo dpkg -i *.deb
 echo "gtp" >> /etc/modules-load.d/modules.conf
 
-cd /root/
-git clone https://gitlab.eurecom.fr/oai/openair-cn
 
-cd /root/openair-cn/SCRIPTS
+"Cloning git repository into /root/openair-cn"
+cd /root/
+#git clone https://gitlab.eurecom.fr/oai/openair-cn
+git clone https://github.com/RuthDevlaeminck/openair-cn.git
+
+# copy prereq script into correct openair folder
+cp $THIS_SCRIPT_PATH/$preq_script /root/openair-cn/SCRIPTS/
+
+# copy buildhelper script into correct openair folder
+cp $THIS_SCRIPT_PATH/$helper_script /root/openair-cn/BUILD/TOOLS/
+
 # we added a prereq script that I am including in the email
-./prereq.sh
-apt install mysql-workbench
+cd /root/openair-cn/SCRIPTS
+
+apt-get update
+apt-get install mysql-workbench -y
+
+# added mysql parameters
+./prereq.sh root root
 ./build_hss
 ./build_mme
 ./build_spgw
-
